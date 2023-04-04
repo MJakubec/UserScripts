562cb110fd8549fd072c6b3047bc160c42c41e8b
Michal Jakubec <mjakubec@asbest.cz>
2023-04-04 11:00:02 +0200

    Add a dataset for topics of subject "Software"

--- ChatGpt/ChatGptPromptSelector.user.js
+++ ChatGpt/ChatGptPromptSelector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Prompt Selector
 // @namespace    https://github.com/MJakubec/UserScripts
-// @version      0.1
+// @version      0.1.1
 // @description  Allows to easily select a prompt from a prepared dataset.
 // @author       Michal Jakubec
 // @updateURL    https://github.com/MJakubec/UserScripts/raw/main/ChatGpt/ChatGptPromptSelector.user.js
@@ -154,6 +154,138 @@
           title: '25. VPN a IPsec',
           mark: 'Question25',
           prompt: 'VPN a IPsec\r\n * účel a výhody použití VPN\r\n * typy VPN\r\n * IPsec - charakteristika'
+        }
+      ]
+    },
+    {
+      title: 'Software - zkoušení otázek',
+      mark: 'SoftwareTraining',
+      prompt: 'Jsi mým zkoušejícím učitelem. V daném kroku mi položíš otázku, na kterou ti napíši odpověď, a ty mi řekneš, zda je má odpověď správná či chybná. V případě správné odpovědi zhodnotíš její úplnost. V případě chybné odpovědi zdůvodníš, proč je chybná, a vysvětlíš mi, jak měla být správně. Nakonec mi položíš další otázku. Téma zkoušení je: """\r\n{prompt}\r\n"""',
+      items: [
+        {
+          title: '1. Operační systémy - základní pojmy, instalace, upgrade a spouštění operačního systému Windows',
+          mark: 'Question01',
+          prompt: 'Operační systémy - základní pojmy, instalace, upgrade a spouštění operačního systému Windows\r\n * účel operačního systému, multiuser OS, multitasking, multiprocessing, multithreading\r\n * základní funkce operačního systému\r\n * možnosti instalace a upgradu operačního systému Windows\r\n * popis spouštěcí sekvence operačního systému Windows, registr Windows'
+        },
+        {
+          title: '2. Rozhraní a základní konfigurace klientské verze operačního systému Windows',
+          mark: 'Question02',
+          prompt: 'Rozhraní a základní konfigurace klientské verze operačního systému Windows\r\n * grafické rozhraní Windows, pracovní plocha, správa souborů a složek\r\n * možnosti správy operačního systému Windows pomocí Ovládacích panelů\r\n * možnosti správy operačního systému Windows pomocí Nástrojů pro správu'
+        },
+        {
+          title: '3. Nástroje pro správu klientské verze operačního systému Windows',
+          mark: 'Question03',
+          prompt: 'Nástroje pro správu klientské verze operačního systému Windows\r\n * systémové nástroje OS Windows\r\n * správa disků v OS Windows\r\n * nástroje příkazového řádku\r\n * možnosti a nástroje pro konfiguraci počítačových sítí ve Windows'
+        },
+        {
+          title: '4. Operační systémy v mobilních zařízeních',
+          mark: 'Question04',
+          prompt: 'Operační systémy v mobilních zařízeních\r\n * základní charakteristika a popis rozhraní OS Android a iOS\r\n * nejběžnější funkce mobilních zařízení\r\n * možnosti zabezpečení mobilních zařízení'
+        },
+        {
+          title: '5. Model klient/server u operačních systémů, operační systém Windows Server',
+          mark: 'Question05',
+          prompt: 'Model klient/server u operačních systémů, operační systém Windows Server\r\n * pojmy server a klient, výhody modelu klient/serveru u operačních systémů, role serveru\r\n * kritéria pro správný výběr serverových komponent, základní subsystémy serveru, virtualizace serveru\r\n * charakteristika operačního systému Windows Server - edice, role a funkce\r\n * možnosti instalace, upgradu a licencování Windows Serveru'
+        },
+        {
+          title: '6. Správa operačního systému Windows Server',
+          mark: 'Question06',
+          prompt: 'Správa operačního systému Windows Server\r\n * aplikace Správce serveru, nástroje pro základní nastavení serveru, pojmy pracovní skupina a doména\r\n * možnosti instalace a správy hardwaru u Windows Serveru\r\n * nástroje pro správu Windows Serveru'
+        },
+        {
+          title: '7. Správa úložišť ve Windows Serveru',
+          mark: 'Question07',
+          prompt: 'Správa úložišť ve Windows Serveru\r\n * serverová úložiště - typy, charakteristika a využití\r\n * disková pole RAID a jejich varianty\r\n * diskové oddíly, typy disků, typy svazků, souborové systémy, správa disků'
+        },
+        {
+          title: '8. Sledování a řešení problémů v operačním systému Windows Server',
+          mark: 'Question08',
+          prompt: 'Sledování a řešení problémů v operačním systému Windows Server\r\n * monitorování činnosti a řešení problémů ve Windows Serveru\r\n * popis procesu spouštění operačního systému Windows Server\r\n * technologie a komponenty používané pro zajištění nepřetržitého provozu serveru\r\n * zálohování a možnosti zotavení serveru'
+        },
+        {
+          title: '9. Adresářová služba Active Directory',
+          mark: 'Question09',
+          prompt: 'Adresářová služba Active Directory\r\n * základní pojmy Active Directory, role FSMO, globální katalog, úroveň funkčnosti\r\n * organizační jednotky, objekty, uživatelé, počítače, skupiny a jejich správa\r\n * zásady skupiny'
+        },
+        {
+          title: '10. Řízení přístupu a správa tisku ve Windows Serveru',
+          mark: 'Question10',
+          prompt: 'Řízení přístupu a správa tisku ve Windows Serveru\r\n * oprávnění na úrovni NTFS, šifrování složek a souborů\r\n * sdílení složek a souborů\r\n * správa místních a síťových tiskáren'
+        },
+        {
+          title: '11. Síťové služby a aplikace ve Windows Serveru',
+          mark: 'Question11',
+          prompt: 'Síťové služby a aplikace ve Windows Serveru\r\n * pojem World Wide Web (WWW), webový server a server FTP ve Windows Serveru\r\n * možnosti podpory více webů ve webovém serveru, virtuální adresář, aplikace, obor aplikací, ověřování a zabezpečení ve webovém serveru\r\n * vzdálený přístup přes VPN, tunelovací protokoly, vzdálená správa serveru\r\n * technologie Hyper-V'
+        },
+        {
+          title: '12. Základní koncepty cloudu a služby Azure',
+          mark: 'Question12',
+          prompt: 'Základní koncepty cloudu a služby Azure\r\n * cloud computing, služba Azure a přehled služeb Azure\r\n * základní koncepty Azure\r\n * základní komponenty architektury Azure'
+        },
+        {
+          title: '13. Popis základních služeb Microsoft Azure',
+          mark: 'Question13',
+          prompt: 'Popis základních služeb Microsoft Azure\r\n * výpočetní služby Azure\r\n * síťové služby Azure\r\n * služby úložiště Azure Storage\r\n * databázové a analytické služby Azure'
+        },
+        {
+          title: '14. Popis základních řešení a nástrojů pro správu v Azure',
+          mark: 'Question14',
+          prompt: 'Popis základních řešení a nástrojů pro správu v Azure\r\n * služby Azure IoT\r\n * služby umělé inteligence v rámci Azure a zásady jejich výběru\r\n * bezserverové technologie Azure\r\n * nástroje a služby pro tvorbu řešení, monitorování, správu a konfiguraci prostředí Azure'
+        },
+        {
+          title: '15. Obecné zabezpečení a zabezpečení sítě v Azure',
+          mark: 'Question15',
+          prompt: 'Obecné zabezpečení a zabezpečení sítě v Azure\r\n * ochrana před bezpečnostními hrozbami v Azure\r\n * detekce a reakce na bezpečnostní hrozby v Azure\r\n * zabezpečení síťového připojení v Azure'
+        },
+        {
+          title: '16. Funkce identity, zásady správného řízení, ochrany osobních údajů a dodržování předpisů v Azure',
+          mark: 'Question16',
+          prompt: 'Funkce identity, zásady správného řízení, ochrany osobních údajů a dodržování předpisů v Azure\r\n * služby identit Azure při zabezpečení přístupu k aplikacím\r\n * zásady správného řízení cloudu v Azure\r\n * standardy ochrany osobních údajů, dodržování předpisů a ochrany dat v Azure'
+        },
+        {
+          title: '17. Cloudová služba Microsoft 365',
+          mark: 'Question17',
+          prompt: 'Cloudová služba Microsoft 365\r\n * základní charakteristika služby Microsoft 365\r\n * nástroje pro produktivitu a týmovou spolupráci v Microsoft 365\r\n * možnosti nasazení a aktualizace aplikací Microsoft 365\r\n * možnosti nástroje Viva Insights, podávání zpráv v Centrum pro správu a portálech Microsoft 365'
+        },
+        {
+          title: '18. Licencování a správa aplikací v UNIX-like operačních systémech',
+          mark: 'Question18',
+          prompt: 'Licencování a správa aplikací v UNIX-like operačních systémech\r\n * licence Open Source a filozofie UNIX-like operačních systémů\r\n * práce s nápovědou\r\n * správci balíčků v UNIX-like operačních systémech'
+        },
+        {
+          title: '19. Sledování využití hardwaru a systémových prostředků v UNIX-like operačních systémech',
+          mark: 'Question19',
+          prompt: 'Sledování využití hardwaru a systémových prostředků v UNIX-like operačních systémech\r\n * monitorování využití prostoru na discích\r\n * monitorování využití operační paměti RAM a CPU\r\n * správa procesů'
+        },
+        {
+          title: '20. Správa úložišť v UNIX-like operačních systémech',
+          mark: 'Question20',
+          prompt: 'Správa úložišť v UNIX-like operačních systémech\r\n * správa oddílů a formátování v UNIX-like operačních systémech\r\n * připojování a používání souborových systémů v UNIX-like operačních systémech\r\n * diskové pole RAID a správa svazků na discích (LVM)'
+        },
+        {
+          title: '21. Správa uživatelů a oprávnění v UNIX-like systémech',
+          mark: 'Question21',
+          prompt: 'Správa uživatelů a oprávnění v UNIX-like systémech\r\n * správa uživatelů a skupin, oprávnění v UNIXu, atributy souborů\r\n * příkaz sudo a jeho konfigurace\r\n * ACL a používání kvót u souborových systémů typu ext'
+        },
+        {
+          title: '22. Konfigurace a správa služeb DNS, DHCP a LDAP v UNIX-like operačních systémech',
+          mark: 'Question22',
+          prompt: 'Konfigurace a správa služeb DNS, DHCP a LDAP v UNIX-like operačních systémech\r\n * konfigurace a správa služby DNS v UNIX-like operačních systémech\r\n * konfigurace a správa služby DHCP v UNIX-like operačních systémech\r\n * konfigurace a správa služby LDAP v UNIX-like operačních systémech'
+        },
+        {
+          title: '23. Konfigurace a správa služby FTP a webového serveru, virtualizace v UNIX-like operačních systémech',
+          mark: 'Question23',
+          prompt: 'Konfigurace a správa služby FTP a webového serveru, virtualizace v UNIX-like operačních systémech\r\n * konfigurace a správa služby FTP v UNIX-like operačních systémech\r\n * konfigurace a správa webového serveru v UNIX-like operačních systémech\r\n * virtualizace a kontejnery'
+        },
+        {
+          title: '24. Kernel, proces zavádění systému, zavaděč GRUB 2',
+          mark: 'Question24',
+          prompt: 'Kernel, proces zavádění systému, zavaděč GRUB 2\r\n * parametry kernel runtime a verze kernelu\r\n * proces zavádění systému v UNIX-like operačních systémech\r\n * zavaděč GRUB 2'
+        },
+        {
+          title: '25. Archivace, zálohování a tvorba repozitáře v UNIX-like operačních systémech',
+          mark: 'Question25',
+          prompt: 'Archivace, zálohování a tvorba repozitáře v UNIX-like operačních systémech\r\n * archivace\r\n * vytváření a správa záloh, zálohování dat\r\n * tvorba repozitáře pro správce balíčků'
         }
       ]
     }
